831bbe6302547e2e9d3487da50ed9f2d79e9feec
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-08-24 18:55:14 +0530

    product display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
+
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
-        <Container><h1>Welcome to Snack Town</h1></Container>
+        <Container><HomeScreen /></Container>
       </main>
       <Footer />
     </>
