a39565548d6ccd2596d0dea2a94a21be4a8c3d6a
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-09-21 09:30:40 +0530

    cleared all warnings

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,29 +1,70 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
     return (
         <div className='rating'>
-            {
-                [1, 2, 3, 4, 5].map((i) => (
-                    <span >
-                        <i style={{ color }}
-                            className={
-                                value >= i
-                                    ? 'fas fa-star'
-                                    :
-                                    value >= ((i - 1) + 0.5)
-                                        ? 'fas fa-star-half-alt'
-                                        :
-                                        'far fa-star'}>
-                        </i>
-                    </span>
-                ))
-            }
             <span>
-                {text && text}
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 1
+                            ? 'fas fa-star'
+                            : value >= 0.5
+                                ? 'fas fa-star-half-alt'
+                                : 'far fa-star'
+                    }
+                ></i>
             </span>
-        </div >
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 2
+                            ? 'fas fa-star'
+                            : value >= 1.5
+                                ? 'fas fa-star-half-alt'
+                                : 'far fa-star'
+                    }
+                ></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 3
+                            ? 'fas fa-star'
+                            : value >= 2.5
+                                ? 'fas fa-star-half-alt'
+                                : 'far fa-star'
+                    }
+                ></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 4
+                            ? 'fas fa-star'
+                            : value >= 3.5
+                                ? 'fas fa-star-half-alt'
+                                : 'far fa-star'
+                    }
+                ></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 5
+                            ? 'fas fa-star'
+                            : value >= 4.5
+                                ? 'fas fa-star-half-alt'
+                                : 'far fa-star'
+                    }
+                ></i>
+            </span>
+            <span>{text && text}</span>
+        </div>
     )
 }
 
@@ -31,9 +72,4 @@ Rating.defaultProps = {
     color: '#f8e825',
 }
 
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string,
-}
 export default Rating
