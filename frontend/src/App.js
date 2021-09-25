2fe354fd781d22efa0307b9f2b7f9b82cf7e3463
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-09-25 11:13:29 +0530

    now product sample can be edited to create desired product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import OrderScreen from "./screens/OrderScreen"
 import UserListScreen from "./screens/UserListScreen"
 import UserEditScreen from "./screens/UserEditScreen"
 import ProductListScreen from "./screens/ProductListScreen"
-
+import ProductEditScreen from "./screens/ProductEditScreen"
 function App() {
   return (
     <Router>
@@ -27,6 +27,7 @@ function App() {
           <Route path="/login" component={LoginScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/productlist" component={ProductListScreen} />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/order/:id" component={OrderScreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
