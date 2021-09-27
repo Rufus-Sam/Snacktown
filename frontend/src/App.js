e69e473946acb110abe3a0ad2c40c834a57b29b7
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-09-27 23:48:41 +0530

    Search box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,7 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
