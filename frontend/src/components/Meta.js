73aa4c35c8a8316753ec0611169a6b491fbd2f84
Rufus-Sam <68532159+Rufus-Sam@users.noreply.github.com>
2021-10-16 12:50:29 +0530

    delete object in s3, favicon,title change

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-    title: 'Welcome To ProShop',
-    description: 'We sell the best products for cheap',
-    keywords: 'electronics, buy electronics, cheap electroincs',
+    title: 'Welcome To SnackTown',
+    description: 'We sell the best tasting snacks for a good price',
+    keywords: 'snacks, binge on till you feel good',
 }
 
 export default Meta
